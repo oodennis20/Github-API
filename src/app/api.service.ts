0677730fdb37d7f8c05c25bebfbc155dfb45b4c7
oodennis20 <oodennis20@gmail.com>
2019-11-02 23:59:48 +0300

    update the api.service.ts

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from 'selenium-webdriver/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
+// getting the info details for the repos
 export class ApiService {
   userName: string;
   apiUrl: string = "https://api.github.com/users/";
 
-  constructor() { }
+  constructor(public http:HttpClient) {
+    this.userName = "oodennis20"
+   }
+
+  getUserDetails(){
+    return this.http.get(this.apiUrl + this.userName + "?access-token" + environment.apiKey)
+  }
+  getRepos() {
+    return this.http.get(this.apiUrl + this.userName + "/repos?access_token=" +  environment.apiKey) 
+  }
+  updateUserName(userName: string) {
+    this.userName = userName;
+  }
 }
