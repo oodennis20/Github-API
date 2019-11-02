63f8e7bf74a43cb8c53b1c8d1b699be7043cc063
oodennis20 <oodennis20@gmail.com>
2019-11-03 00:35:03 +0300

    add changes to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MasterComponent } from './master/master.component';
+import { ApiService } from './api.service';
 import { HighlightDirective } from './highlight.directive';
+import { RoutingModule } from './routing/routing.module';
+import { RepoComponent } from './repo/repo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MasterComponent,
-    HighlightDirective
+    HighlightDirective,
+    RepoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RoutingModule,
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
