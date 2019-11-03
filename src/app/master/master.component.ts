be009eb663f6790080a4e63012c48373c3bdfb0f
oodennis20 <oodennis20@gmail.com>
2019-11-03 19:06:29 +0300

    add import Router to the ts for the master component

--- src/app/master/master.component.ts
+++ src/app/master/master.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../api.service'
+import { ApiService } from '../api.service' ;
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-master',
   templateUrl: './master.component.html',
@@ -7,8 +9,35 @@ import { ApiService } from '../api.service'
 })
 export class MasterComponent implements OnInit {
 
-  constructor() { }
+  user1;
+  repos;
+  userName: string;
 
+
+  constructor( public apiservice: ApiService, private router: Router ) {
+
+
+    this.apiservice.getUserDetails().subscribe(users => {
+      this.user1 = users;
+      console.log(this.user1);
+    });
+    this.apiservice.getRepos().subscribe(result => {
+      this.repos = result;
+      console.log(this.repos);
+    });
+  }
+
+  findUser() {
+    this.apiservice.updateUserName(this.userName);
+    this.apiservice.getUserDetails().subscribe(users => {
+      this.user1 = users;
+      console.log(this.user1);
+    });
+    this.apiservice.getRepos().subscribe(result => {
+      this.repos = result;
+      console.log(this.repos);
+    });
+  }
   ngOnInit() {
   }
 
--- src/app/repo/repo.component.ts
+++ src/app/repo/repo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-repo',
@@ -6,8 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repo.component.css']
 })
 export class RepoComponent implements OnInit {
+  
+  user1;
+  repos;
+  userName: string;
 
-  constructor() { }
+  constructor(public apiservice: ApiService) { 
+    this.apiservice.getUserDetails().subscribe(users => {
+      this.user1 = users;
+      console.log(this.user1);
+    });
+    this.apiservice.getRepos().subscribe(users => {
+      this.user1 = users;
+      console.log(this.repos);
+    });
+  }
+
+  findUser() {
+    this.apiservice.updateUserName(this.userName);
+    this.apiservice.getUserDetails().subscribe(users => {
+      this.user1 = users;
+      console.log(this.user1);
+    });
+    this.apiservice.getRepos().subscribe(result => {
+      this.repos = result;
+      console.log(this.repos);
+    });
+  }
 
   ngOnInit() {
   }
